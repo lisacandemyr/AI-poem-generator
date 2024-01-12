53d8ffe92080d6ec03b17fc31ca36b202b4cf9ad
lisacandemyr <141151710+lisacandemyr@users.noreply.github.com>
2024-01-12 17:29:49 +0000

    added API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+function displayPoem(response) {
+  console.log("Poem generated");
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
   new Typewriter("#poem", {
-    strings: "Poem will show here",
+    strings: ["Generating poem.."],
     autoStart: true,
     cursor: null,
-    delay: 20,
+    delay: 1,
   });
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "344027118o9t0bba5e252d5b7747161f";
+  let context =
+    "You are a romantic poem writer. Your mission is to generate a 4 line poem in basic HTML. Do not include a title in the poem. Sign the poem with `SheCodes AI` inside a <strong> element and include at the end of the poem on it's own line. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemElement = document.querySelector("#poem-generator-form");
